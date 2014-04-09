74a7def3d7f61f4ecbe2cea281a5d6437e2f30b9
Philipp Fehre <philipp.fehre@googlemail.com>
2014-04-09 11:49:44 +0100

    JSCBC-104 implement bucket flush
    Implementing bucket flush
    
    * added mocha globals to jshintrc
    * added missing POST constant export
    * extend mocha timeout to prevent tests from failing on slow machines
    * make the bucket name accessible in the Connection object
    * handle empty responses for put and post requests http requests
    * ignore local test configuration (config.json)
    
    Change-Id: I4fb213ab71644f49546ae6a47357fe4cc9f71c12

---
+++ test/flush.js
@@ -0,0 +1,25 @@
+'use strict';
+var assert = require('assert');
+var H = require('../test_harness');
+var util = require('util')
+
+var cb = H.newClient();
+
+describe('#flush', function() {
+  var key;
+  before(function(done) {
+    key = H.genKey("set")
+    cb.set(key, "bar", done)
+  });
+
+  it('should remove all the keys successfully', function(done) {
+    cb.flush(function(err) {
+      assert.ifError(err);
+      cb.get(key, function(err, res) {
+        assert.equal(err.code, H.errors.keyNotFound);
+        done();
+      });
+    });
+  })
+});
+
